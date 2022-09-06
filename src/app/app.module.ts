ab92958e74f14ff6ec0025c66acac77c5995cf04
Juan Paz <jjfer032@gmail.com>
2022-09-06 09:05:51 -0500

    creacion de navbar principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { ListOfProductsComponent } from './components/list-of-products/list-of-products.component';
 import { SingleProductComponent } from './components/single-product/single-product.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListOfProductsComponent,
-    SingleProductComponent
+    SingleProductComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
